f1ec15a29f6eddcc7b7da6147b43ff02f3e2bdce
Felipe Fazan <99650439+ffaZan@users.noreply.github.com>
2022-08-12 11:36:21 -0300

    Add files via upload

---
+++ aula.java
@@ -0,0 +1,26 @@
+public class Main
+{
+  public static void main (String[]args)
+  {
+      
+      int valor1 = 50;
+      int valor2 = 5;
+
+// OPERADORES ARITMÉTICOS
+int resultado1 = valor1 + valor2;
+System.out.println("Soma de valor1 e valor2:" + resultado1);
+
+int resultado2 = valor1 - valor2;
+System.out.println("Subtração de valor1 e valor2: " + resultado2);
+
+int resultado3 = valor1 * valor2;
+System.out.println("Multiplicação de valor1 e valor2: " + resultado3);
+
+int resultado4 = valor1 / valor2;
+System.out.println("Divisão de valor1 e valor2:" + resultado4);
+
+int resultado5= valor1 % valor2;
+System.out.println("Resto da Divisão:" + resultado5);
+
+  }
+}
